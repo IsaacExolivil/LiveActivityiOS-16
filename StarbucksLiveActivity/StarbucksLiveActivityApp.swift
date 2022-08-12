a64efe18ddfe641b72744b3cc75d7b1715835965
isaaclopez <exolivil@gmail.com>
2022-08-12 14:51:37 -0500

    Initial Commit

---
+++ StarbucksLiveActivity/StarbucksLiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksLiveActivityApp.swift
+//  StarbucksLiveActivity
+//
+//  Created by Jose Isaac on 12/08/22.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksLiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
